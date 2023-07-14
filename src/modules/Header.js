8e261473ef6c691253a6ca676ecc4de896ebaa61
Hassan Cheikh-Hussein <46470134+hasshuss@users.noreply.github.com>
2023-07-14 18:55:00 +0200

    Add files via upload

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import '../App.scss';
-import LOGO from '../images/LOGO.png'
-import {NavLink} from 'react-router-dom'
+import LOGO from '../images/LOGO.png';
+import { NavLink } from 'react-router-dom';
 
+const Header = () => {
+  return (
+    <header>
+      <NavLink to='/'>
+      <img src={LOGO} alt="logo" />
+      </NavLink>
+      <nav>
+        <ul>
+          <li>
+            <NavLink activeclassname="active" to="/" exact="true">
+              Acceuil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeclassname="active" to="/About" exact="true">
+              A propos
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-  const Header = () => {
-    return (
-      <header>
-        <img src={LOGO} alt="logo" />
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Acceuil</NavLink>
-            </li>
-            <li>
-              <a href="/About">A propos</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  };
-  
-  export default Header;
+export default Header;
